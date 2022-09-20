d03aa6849172bb65e42cd87547b759a8ad81b73c
Diba <88105499+dee-ba@users.noreply.github.com>
2022-09-20 04:47:37 +0200

    commit

--- resources/views/items/view.blade.php
+++ resources/views/items/view.blade.php
@@ -3,47 +3,22 @@
 @section ('content')
 
 
-<h1>Hello</h1>
 
+    <table>
+        <tr>
+            <td> <strong> {{$item->name}} </strong> </td> 
+        </tr> 
 
+        <tr>
+           <td> {{$item->detail}} </td> 
+        </tr>         
+         
+        <tr>
+            <td><img width= "500px" src="{{ $item->image }}"></td>
+        </tr>
 
-
-<table>
-            <tr>
-              <th>Checkbox</th>
-              <th>Image</th>
-              <th>Name</th>   
-              <th>Detail</th>
-              <th>Edit</th>
-              <th>Delete</th>
-            </tr>
-
-           
-            <tr>
-              <th width="50px"><input type="checkbox"></th>
-              </th>
-              <td><img width= "100px" src="{{ $item->image }}"></td>
-              <td>{{$item->name}}</td>
-              <td>{{$item->detail}}</td>
-
-              <td> 		
-                  <button>
-                      <a href="{{'view/' .$item['id'] }}">View</a>
-                  </button>
-              </td>
-
-              <td> 		
-                  <button>
-                      <a href="{{'edit/' .$item['id'] }}">Edit</a>
-                  </button>
-              </td>
-              <td>
-                <button>
-                    <a href="{{'delete/' .$item['id'] }}"> Delete </a>
-                </button>
-              </td>
-            
-            
+         
+  
       </table>
 
 
